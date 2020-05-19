b7187584bc726b658a7baac8aaffbabb1b959914
Dave McNamara <mcnamaracommadavid@gmail.com>
2020-05-19 11:25:33 -0400

    start

--- app.js
+++ app.js
@@ -11,6 +11,49 @@ var updaterRouter = require('./routes/updater');
 
 var app = express();
 
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://davem:" + process.env.MONGOKEY + "@studentdata-8hxes.gcp.mongodb.net/test?retryWrites=true&w=majority&family=4";
+
+var passport = require('passport');
+var Strategy = require('passport-local').Strategy;
+
+MongoClient.connect(uri, function (err, client) {
+  if (err) {
+    console.log('Error occurred while connecting to MongoDB Atlas...\n', err);
+  }
+  console.log('Connected...');
+  const collection = client.db("all").collection("allstudents");
+
+  let loginPromise = new Promise((resolve, reject) => {
+    collection.find().toArray((err, results) => {
+      var logindict = {};
+      for (i in results) {
+        var loginname = results[i].name;
+        var password;
+        if (results[i].password == undefined) {
+          password = 'steinway';
+        }
+        else {
+          password = results[i].password;
+        }
+        logindict[loginname] = password;
+      }
+      client.close();
+      resolve(logindict);
+      reject('logindict promise came up empty')
+    });
+
+  });
+  loginPromise.then((logindict) => {
+    for (var user in logindict) {
+      console.log(user);
+      console.log(logindict[user]);
+    }
+  });
+
+
+});
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
+
+// passport.use(new Strategy(
+//     function(username, password, cb) {
+//         db.users.findByUsername(username, function(err, user) {
+//             if (err) { return cb(err); }
+//             if (!user) { return cb(null, false); }
+//             if (user.password != password) { return cb(null, false); }
+//             return cb(null, user);
+//         });
+//     }));
+
 /* GET home page. */
 router.get('/', function (req, res, next) {
 
