a8cebb6d361e3c97ecd822c08c2c8af94d2623b7
Dave McNamara <mcnamaracommadavid@gmail.com>
2020-05-19 16:13:25 -0400

    oooooooooops

--- app.js
+++ app.js
@@ -81,7 +81,7 @@ app.use(function (err, req, res, next) {
     res.locals.error = req.app.get('env') === 'development' ? err : {};
 
     res.status(err.status || 500);
-    res.render('error');
+    res.render('index', {title: 'Student Portal', error: ''});
 });
 
 module.exports = app;
